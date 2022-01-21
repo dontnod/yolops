a52557a09ad5d4019c3963f7866d024bf3b06299
Sam Hocevar <sam@hocevar.net>
2022-01-21 15:09:55 +0100

    Add --delete and --keep arguments to the expire-cache command

--- yolops/commands/expire_cache.py
+++ yolops/commands/expire_cache.py
@@ -9,6 +9,7 @@ import click
 from yolops.minmaxheap import MinMaxHeap
 from yolops.parsing import StorageUnit
 
+
 def scantree(directory):
     """
     Scan a directory tree recursively, returning entries for files
@@ -18,6 +19,26 @@ def scantree(directory):
             yield from scantree(entry.path)
         else:
             yield entry
+
+
+class Scanner(object):
+
+    def __init__(self, policy: str):
+        if policy == 'MRU':
+            self._make_key = lambda stat: -stat.st_mtime
+        elif policy == 'random':
+            self._make_key = lambda stat: random()
+        else: # 'LRU'
+            self._make_key = lambda stat: stat.st_mtime
+
+    def scan(self, directory: str):
+        for entry in scantree(directory):
+            try:
+                stat = entry.stat()
+                yield self._make_key(stat), stat.st_size, entry.path
+            except FileNotFoundError:
+                pass
+
 
 def unit(size: int):
     """
@@ -32,72 +53,81 @@ def unit(size: int):
     return f'{mantissa:1.3g}{unit}B'
 
 @click.command()
-@click.option('-f', '--ensure-free', 'ensure_free', type=StorageUnit(), default=0,
+@click.option('-d', '--delete', 'todelete_size', type=StorageUnit(), default=-1,
+              help='delete SIZE bytes of data')
+@click.option('-f', '--ensure-free', type=StorageUnit(), default=-1,
               help='ensure at least SIZE bytes are available on the filesystem')
+@click.option('-m', '--keep', 'tokeep_size', type=StorageUnit(), default=-1,
+              help='ensure directory uses at most SIZE bytes')
 @click.option('--lru',    'policy', flag_value='LRU',    help='remove oldest files (default)', default=True)
 @click.option('--mru',    'policy', flag_value='MRU',    help='remove newest files')
 @click.option('--random', 'policy', flag_value='random', help='remove files randomly')
 @click.option('-v', '--verbose', is_flag=True, help='verbose output')
 @click.option('-n', '--dry-run', is_flag=True, help='perform a trial run with no changes made')
 @click.argument('directory', type=click.Path(file_okay=False, dir_okay=True, resolve_path=True))
-def expire_cache(directory: str, ensure_free: int, verbose: bool, dry_run: bool, policy: str):
+def expire_cache(directory: str, todelete_size: int,  ensure_free: int, tokeep_size: int,
+                 verbose: bool, dry_run: bool, policy: str):
+
+    # Check argument consistency
+    if len([x for x in [tokeep_size, ensure_free, todelete_size] if x >= 0]) != 1:
+        raise click.UsageError('You must specify one of --delete, --ensure-free, or --keep')
 
     click.echo(f'Expiring files in {directory} (policy: {policy})')
 
+    # Gather stats about the filesystem
     diskinfo = disk_usage(path=directory)
     total, free, used = diskinfo.total, diskinfo.free, diskinfo.used
     click.echo(f'Filesystem size is {unit(total)} total, {unit(used)} used, {unit(free)} free')
 
-    if ensure_free <= free:
+    # Handle --ensure-free: it’s the same as --delete except the actual value
+    # is computed from the filesystem stats
+    if ensure_free >= 0:
+        todelete_size = ensure_free - free
+        tokeep_size = -1
+
+    if todelete_size < 0 and tokeep_size < 0:
         click.echo('Nothing to do!')
         return True
 
-    excess_size = ensure_free - free
-
-    if policy == 'MRU':
-        make_key = lambda stat: -stat.st_mtime
-    elif policy == 'random':
-        make_key = lambda stat: random()
-    else: # 'LRU'
-        make_key = lambda stat: stat.st_mtime
-
     # Scan all files into a min-max heap, sorted by modification date (oldest first)
+    scanner = Scanner(policy)
     heap = MinMaxHeap()
     tracked_size = 0
     discovered_size = 0
     discovered_count = 0
-    for entry in scantree(directory):
-        try:
-            stat = entry.stat()
-            heap.insert((make_key(stat), stat.st_size, entry.path))
-            tracked_size += stat.st_size
-            discovered_size += stat.st_size
-            discovered_count += 1
-        except FileNotFoundError:
-            pass
-        # If we’re tracking more data than we need to clean, we can ignore the most recent files
-        while tracked_size > excess_size:
+    freed_size = 0
+    freed_count = 0
+    for key, size, path in scanner.scan(directory):
+        heap.insert((key, size, path))
+        tracked_size += size
+        discovered_size += size
+        discovered_count += 1
+        # If we need to keep at most X bytes and we already found more than that, we can start deleting
+        while tokeep_size >= 0 and tracked_size > tokeep_size:
+            _, size, path = heap.popmin()
+            if verbose:
+                click.echo(f'Deleting file: {path} ({unit(size)})')
+            if not dry_run:
+                unlink(path)
+            tracked_size -= size
+            freed_size += size
+            freed_count += 1
+        # If we already found more data than we need to delete, we can stop tracking
+        # some of these files because we know we will never have to delete them
+        while todelete_size > 0 and tracked_size > todelete_size:
             _, size, _ = heap.popmax()
             tracked_size -= size
 
-    click.echo(f'Found {unit(discovered_size)} of data ({discovered_count} files)')
+    click.echo(f'Scanned {discovered_count} files ({unit(discovered_size)})')
 
     # Delete files until we reach the threshold
-    freed_size = 0
-    freed_count = 0
-    while freed_size < excess_size and len(heap):
+    while freed_size < todelete_size and len(heap):
         key, size, path = heap.popmin()
         if verbose:
-            if policy == 'MRU':
-                info = datetime.fromtimestamp(-key)
-            if policy == 'random':
-                info = 'random'
-            else: # Policy.LRU
-                info = datetime.fromtimestamp(key)
-            click.echo(f'Deleting file: {path} ({info}, {unit(size)})')
+            click.echo(f'Deleting file: {path} ({unit(size)})')
         if not dry_run:
             unlink(path)
         freed_size += size
         freed_count += 1
 
-    click.echo(f'Freed {unit(freed_size)} of data ({freed_count} files)')
+    click.echo(f'Deleted {freed_count} files ({unit(freed_size)})')
