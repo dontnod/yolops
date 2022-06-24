fa0386bb69f6b6a8b39cb9a58ec2f1fee20de0d8
Sam Hocevar <sam@hocevar.net>
2022-06-24 10:19:42 +0200

    Preserve file creation date and ownership (if possible) when compressing files

---
+++ yolops/fsutils.py
@@ -0,0 +1,23 @@
+def gzip_file(src: str, dst: str):
+    ''' Compress a file using the standard gzip method. Try to preserve file creation
+        date and other metadata. '''
+
+    from os import chown, stat, unlink
+    from shutil import copyfileobj, copystat
+    from gzip import open as gzip_open
+
+    # Compress file
+    with open(src, 'rb') as fin:
+        with gzip_open(dst, 'wb') as fout:
+            copyfileobj(fin, fout)
+
+    # Copy file metadata
+    copystat(src, dst)
+    try:
+        st = stat(src)
+        chown(dst, st.st_uid, st.st_gid)
+    except:
+        pass
+
+    # Remove the source file if everything went well
+    unlink(src)
